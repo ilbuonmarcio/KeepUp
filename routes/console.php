323be22cc97c69f2e28bb129da4f06bd918dd3de
ilbuonmarcio <alessandro@goldmark.solutions>
2025-07-10 18:31:56 +0200

    Added weekly mail to report monitors, updated README

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:monitor')->dailyAt('08:00');
-Schedule::command('app:send-email-monitor-recap')->dailyAt('10:30');
+Schedule::command('app:send-email-monitor-recap')->weekly();
